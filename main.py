5ed486b05c32fba38a94fa01189c74be7a566a3d
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-28 18:38:41 -0400

    Set up receiver class

---
+++ main.py
@@ -0,0 +1,94 @@
+from ares_iq.signal_hound import SM200C, SM435C, SMConfigs, GpsModel, sm_get_device_list, SmDevice, GpsState
+from ares_lora import LoraSerial, LoraException, LoraSerialConfig, LoraConfig, LoraLedState, LoraCodingRate, \
+    LoraSpreadingFactor, LoraBandwidth
+import threading
+import time
+from datetime import timedelta
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, Future
+
+
+class AresReceiver:
+    def __init__(self, lora_port: str, gps_stamping: bool, model: GpsModel = GpsModel.STATIONARY):
+        lora_configs = LoraSerialConfig(
+            port=lora_port,
+            start_callback=self._lora_start_cb
+        )
+
+        self._lora_dev = LoraSerial(lora_configs)
+        self._lora_dev.start_driver()
+        self._dev_ready = threading.Event()
+        self._heartbeat_lock = threading.Lock()
+        self._heartbeat_running = False
+
+        sm_class = self._get_dev_class()
+        self._sm_dev = sm_class(SMConfigs(gps_timestamping=gps_stamping, gps_model=model.value))
+        self._gps_stamping = gps_stamping
+
+        self._tasks: ThreadPoolExecutor = ThreadPoolExecutor(max_workers=2)
+        self._lora_heartbeat_future: Future[None] | None = None
+
+        self._start_signal = threading.Event()
+        self._start_time_sec: int = 0
+        self._start_time_ns: int = 0
+
+    @staticmethod
+    def _get_dev_class() -> type[SM200C | SM435C]:
+        devices = sm_get_device_list(usb=False, max_network_devices=1)
+        if devices[0].type == SmDevice.type.SM200C:
+            print("SM200C found")
+            return SM200C
+        elif devices[0].type == SmDevice.type.SM435C:
+            print("SM435C found")
+            return SM435C
+        else:
+            raise RuntimeError("SM device not found")
+
+    def _lora_heartbeat(self):
+        while self._heartbeat_running:
+            with self._heartbeat_lock:
+                ready = self._dev_ready.is_set()
+                self._lora_dev.send_heartbeat(ready)
+            time.sleep(1.0)
+
+    def _lora_start_cb(self, seconds: int, nanoseconds: int):
+        self._start_time_sec = seconds
+        self._start_time_ns = nanoseconds
+        self._start_signal.set()
+
+    def capture_data(self, center: float, bw: float, duration: timedelta, save_directory: str | Path):
+        self._sm_dev.open()
+        if self._gps_stamping:
+            self._sm_dev.acquire_gps_lock(GpsState.DISCIPLINED)
+        self._dev_ready.set()
+
+        self._start_signal.wait()
+
+        with self._heartbeat_lock:
+            # TODO: Wait until specified time
+            self._sm_dev.stream_iq(center, bw, int(4e9), duration, save_directory)
+            self._dev_ready.clear()
+        
+        self._start_signal.clear()
+
+    def start(self):
+        if self._heartbeat_running:
+            raise RuntimeError("Already running")
+        self._heartbeat_running = True
+        self._lora_heartbeat_future = self._tasks.submit(self._lora_heartbeat)
+
+    def stop(self):
+        if not self._heartbeat_running:
+            raise RuntimeError("already stopped")
+        self._heartbeat_running = False
+        assert self._lora_heartbeat_future is not None
+        self._lora_heartbeat_future.result()
+
+    def __del__(self):
+        try:
+            self.stop()
+        except RuntimeError:
+            pass
+        self._tasks.shutdown()
+        self._sm_dev.close()
+        self._lora_dev.stop_driver()
