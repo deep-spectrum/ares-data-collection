59a862b405ca4ca6d4b8332bad5210a7e8a47d94
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-06-12 18:40:01 -0400

    Fuck Python

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class AresReceiver:
         self._start_signal.clear()
 
     def start(self):
-        if self._heartbeat_running:
+        if self._heartbeat_running.is_set():
             raise RuntimeError("Already running")
         self._heartbeat_running.set()
         self._heartbeat_thread = threading.Thread(target=_lora_heartbeat, args=[self._heartbeat_running, self._heartbeat_lock, self._dev_ready, self._lora_dev])
@@ -88,7 +88,7 @@ class AresReceiver:
         self._heartbeat_thread.start()
 
     def stop(self):
-        if not self._heartbeat_running:
+        if not self._heartbeat_running.is_set():
             raise RuntimeError("already stopped")
         self._lora_dev.set_logging_level(logging.DEBUG)
         self._heartbeat_running.clear()
