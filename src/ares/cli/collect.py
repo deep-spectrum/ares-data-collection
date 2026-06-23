abbd295e3964ee3e0dc008b71220a4af6da8f7e7
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-06-23 18:26:20 -0400

    Add save path magic

--- src/ares/cli/collect.py
+++ src/ares/cli/collect.py
@@ -1,7 +1,9 @@
 import typer
 from typing_extensions import Annotated
 from ares.receiver import AresReceiver
-from datetime import timedelta
+from datetime import timedelta, datetime
+from .configure import get_setting, Configuration
+from pathlib import Path
 
 
 app = typer.Typer()
@@ -24,11 +26,23 @@ def collect(
 
     rx = AresReceiver(lora_port, gps_timestamping)
     rx.start()
+    rx_id = rx.node_id
 
-    some_path = ""
+    save_path = Path(get_setting(Configuration.save_location))
+    if not save_path.exists():
+        print(f"{save_path} does not exist.")
+        raise typer.Exit(1)
+
+    now = datetime.now()
+    date_string = now.strftime("%Y-%m-%d-%H-%M-%S")
+    save_path = save_path / date_string
+    save_path.mkdir()
+
+    save_path = save_path / f"rx{rx_id}"
+    save_path.mkdir()
 
     try:
-        rx.capture_data(center, bandwidth, timedelta(seconds=duration), some_path, quiet)
+        rx.capture_data(center, bandwidth, timedelta(seconds=duration), save_path, quiet)
     except KeyboardInterrupt:
         rx.stop()
         print("No data captured")
