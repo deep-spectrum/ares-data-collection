d7622e11c36db879d35962d4ad347a76e58504b6
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-06-26 18:24:56 -0400

    Now generating image

--- src/ares/cli/spot_check.py
+++ src/ares/cli/spot_check.py
@@ -2,11 +2,12 @@ import tyro
 from typing_extensions import Annotated
 from ares.receiver import AresReceiver
 from typing import Literal
-from datetime import timedelta, datetime
+from datetime import datetime
 from pathlib import Path
 from ares.spectrogram import SpectrogramParameters, generate_spectrogram, Window
 from tempfile import TemporaryDirectory
 import numpy as np
+import shutil
 
 
 def spot_check(
@@ -36,34 +37,32 @@ def spot_check(
     now = datetime.now()
     iq = rx.capture_live_data(center, bandwidth, capture_size)
 
-    iq_data = np.vstack([iq_.iq for iq_ in iq])
-    ts_data = np.array([iq_.ts.timestamp() for iq_ in iq])
+    iq_data = np.vstack([iq_.iq for iq_ in iq], dtype=np.complex64)
+    ts_data = np.array([iq_.ts.timestamp() for iq_ in iq], dtype=np.float64)
 
-    print(iq_data)
-    print(ts_data)
+    with TemporaryDirectory() as temp:
+        name = f"capture {now.strftime('%Y-%m-%d-%H%M%S')}"
+        file = Path(temp) / name
 
-    # with TemporaryDirectory() as temp:
-    #     name = f"capture {now.strftime('%Y-%m-%d-%H%M%S')}"
-    #
-    #     params = SpectrogramParameters(
-    #         Path(temp) / name,
-    #         center,
-    #         bandwidth,
-    #         -20,
-    #         rx.sample_rate,
-    #
-    #         nfft=nfft,
-    #         window=window_enum[window],
-    #         colormap=colormap,
-    #         max_rows=max_rows,
-    #         node_id=rx.node_id,
-    #         show=show,
-    #         dpi=dpi,
-    #     )
-    #
-    #
-    #
-    #     # generate_spectrogram(iq.)
-    #
-    #     if save_loc is not None:
+        params = SpectrogramParameters(
+            file,
+            center,
+            bandwidth,
+            -20,
+            rx.sample_rate,
 
+            nfft=nfft,
+            window=window_enum[window],
+            colormap=colormap,
+            max_rows=max_rows,
+            node_id=rx.node_id,
+            show=show,
+            dpi=dpi,
+        )
+
+
+
+        generate_spectrogram(iq_data, ts_data, params)
+
+        if save_loc is not None:
+            shutil.copy(file, save_loc)
